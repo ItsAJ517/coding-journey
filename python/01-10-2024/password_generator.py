6ab359be1e6cc4e2bc7b8f6cf6f5c3e95830cd1c
ItsAJ517 <agastyajain0508@gmail.com>
2025-04-26 23:36:07 +0100

    Update password_generator.py

--- python/01-10-2024/password_generator.py
+++ python/01-10-2024/password_generator.py
@@ -1,1 +1,16 @@
 import random
+
+print("Password Generator")
+# User inputs
+pet_name = input("\nEnter your pet's name: ").upper()
+holiday = input("Enter your favourite holiday destination: ").upper()
+house_num = input("Enter your house number: ")
+birth_year = input("Enter your birth year: ")
+
+# Variables
+prefix = random.randint(0, int(house_num) + int(birth_year))
+suffix = int(house_num) + int(birth_year)
+password = str(prefix) + holiday.upper() + pet_name.upper() + str(suffix)
+
+print("Here is your password: ")
+input(password)
