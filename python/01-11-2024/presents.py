8ab61cf61bad587093d08b36bcc9c05cb0789f78
ItsAJ517 <agastyajain0508@gmail.com>
2025-04-28 17:19:43 +0100

    Update presents.py

--- python/01-11-2024/presents.py
+++ python/01-11-2024/presents.py
@@ -1,1 +1,23 @@
+print("Presents")
 
+print("\nYou have £200 to spend on presents.")
+print("I will ask you to input the price of each present, until the total exceeds £200.")
+print("You can have as many presents as you want, as long as you don't exceed £200.")
+
+total = 0
+
+while total <= 200:
+    price = float(input("\nEnter the price of your present: £"))
+    
+    if total + price <= 200:
+        total += price
+        
+        if total == 200:
+            break
+        print(f"Total so far: £{total}")
+        
+    elif total + price > 200:
+        print(f"Limit exceeded. You cannot include that £{price} present.")
+
+print("You have spent £200!")
+input("Press enter to close the program.")
