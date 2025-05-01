8153ef5a0324d97d976ffbe18f20f562489c3016
ItsAJ517 <agastyajain0508@gmail.com>
2025-05-01 18:06:56 +0100

    Update GalToLitTable.java

--- java/2025/February 2025/GalToLitTable.java
+++ java/2025/February 2025/GalToLitTable.java
@@ -1,1 +1,26 @@
+/*
+ * Gallons to litres converter
+ * This program converts gallons to litres, from 1 - 100.
+ */
 
+ class GalToLitTable {
+    public static void main(String[] args) {
+        // Initialize variables
+        double gallons, litres;
+        int counter;
+
+        counter = 0;
+
+      // Begin conversions
+        for(gallons = 1; gallons <= 100; gallons++) {
+            litres = gallons * 3.7854;
+            System.out.println(gallons + " gallons is " + litres + " litres");
+
+            counter++;
+            if(counter == 10) {
+                System.out.println();
+                counter = 0;
+            }
+        }
+    }
+}
