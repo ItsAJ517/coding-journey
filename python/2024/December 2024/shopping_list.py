5d7e97592cfc69e79f53239bda84fce9e1322e5a
ItsAJ517 <agastyajain0508@gmail.com>
2025-05-01 18:26:18 +0100

    Update shopping_list.py

--- python/2024/December 2024/shopping_list.py
+++ python/2024/December 2024/shopping_list.py
@@ -8,10 +8,12 @@
 
 shopping_items = []
 
+# Welcome message
 print("Shoppping List Program\n")
 
 # Main program begins here
 while True:
+    # User enters option
     option = input("""\nEnter (1) Add item
       (2) Delete item
       (3) View items
@@ -20,22 +22,24 @@ Option: """)
     
     #[1] Add item
     if option == "1":
+        # User enter item
         item = input("Enter the item you would like to add: ")
         
-        # Selection statements used to avoid errors
+        # Check if item is in the shopping list to avoid duplicate items
         if item in shopping_items:
             print(f"'{item}' is already in your shopping list")
         else:
-            shopping_items.append(item)
+            shopping_items.append(item) # add item
             print("Item added succesfully")
 
     #[2] Remove item
     elif option == "2":
+        # User enters item
         item = input("Enter the item you would like to remove: ")
 
-        # Selection statements used to avoid errors
+        # Check if item is in the shopping list to avoid errors
         if item in shopping_items:
-            shopping_items.remove(item)
+            shopping_items.remove(item) # remove item
             print("Item removed succesfully")
         else:
             print("Item not in list")
@@ -46,13 +50,16 @@ Option: """)
     
     #[0] Exit
     elif option == "0":
-        break
+        break # end loop
     
     #[X] Invalid inputs
     else:
+        # Tell user to enter correct option
         print("Invalid choice")
 
 
 input("Press <Enter> to close the program")
 
+# Insights: I was using a loop control variable for my 'while' loop before. However, my teacher pointed out that using an endless loop is easier.
+# My program was also much longer before as I had the 'option' input variable inside each selection statement rather than at the top of the loop
 # Improvements: when viewing the shopping list, I could have formatted it in a nicer way.
